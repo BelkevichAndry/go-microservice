aed874f98183a9211514cc6e0be0cf58f77db487
belkevichandry@gmail.com <belkevichandry@gmail.com>
2020-07-04 16:03:12 +0300

    update proto file

--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -6,19 +6,17 @@ import (
 	"net"
 	"sync"
 
-	// Import the generated protobuf code
 	pb "github.com/BelkevichAndry/go-microservice/shippy-service-consignment/proto/consignment"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 )
 
 const (
 	port = ":50051"
 )
 
-
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
@@ -35,6 +33,11 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	repo.consignments = updated
 	repo.mu.Unlock()
 	return consignment, nil
+}
+
+// GetAll consignments
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
 }
 
 // Service should implement all of the methods to satisfy the service
@@ -61,6 +64,12 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	return &pb.Response{Created: true, Consignment: consignment}, nil
 }
 
+// GetConsignments -
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
+}
+
 func main() {
 
 	repo := &Repository{}
@@ -75,13 +84,7 @@ func main() {
 	// Register our service with the gRPC server, this will tie our
 	// implementation into the auto-generated interface code for our
 	// protobuf definition.
-
-
 	pb.RegisterShippingServiceServer(s, &service{repo})
-
-
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
 
 	log.Println("Running on port:", port)
 	if err := s.Serve(lis); err != nil {
