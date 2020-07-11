dd9b74ecd711edfaf38d4bcec1d8fd77cbc4814e
belkevichandry@gmail.com <belkevichandry@gmail.com>
2020-07-11 13:08:01 +0300

    add vessel service

--- shippy-service-vessel/main.go
+++ shippy-service-vessel/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"context"
 	"errors"
-	"github.com/micro/micro/v2/cmd/protoc-gen-micro/plugin/micro"
 	"log"
 
 	pb "github.com/BelkevichAndry/go-microservice/shippy-service-vessel/proto/vessel"
+
+	"github.com/micro/go-micro/v2"
 )
 
 type Repository interface {
